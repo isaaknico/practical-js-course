4bed03014bdc4de3c909eba37a41ae86bd2f035b
isaaknico <kaasi_96@hotmail.com>
2022-06-11 14:27:50 -0500

    Añadí calcular altura de triangulo

--- figures.js
+++ figures.js
@@ -116,3 +116,60 @@ function calcularAreaCirculo() {
     const area = areaCirculo(radio);
     alert(area);
 }
+
+
+/**** RETO  ****/
+// Durante este taller aprendimos a calcular el área y perímetro de un triángulo
+// conociendo la longitud de sus 3 lados y también su altura.
+
+// En este ejercicio debes crear una función para calcular la altura de un triángulo isósceles.
+
+// La función debe recibir, como parámetros, la longitud de los 3 lados del triángulo.
+// La función debe validar que la longitud de los 3 lados del triángulo corresponden a un triángulo isósceles.
+// La función debe retornar la altura del triángulo.
+// Pista: la función Math.sqrt de JavaScript puede ayudarte a calcular raíces cuadradas.
+
+function alturaTriangulo(base, lado1, lado2) {
+    /* 
+    if (lado1 == lado2) {
+        if (lado1 == base) {
+            console.log("es equilatero");
+        } else {
+            console.log("es isoceles");
+        }
+    } else if (lado1 == base || lado2 == base) {
+        console.log("es isoceles");
+    } else {
+        console.log("es escaleno");
+    } */
+
+    if (lado1 == lado2 && lado1 == base) {
+        console.log("es equilaterov2");
+        const altura = Math.sqrt((lado1**2) - ((lado1 / 2)**2));
+        return Number(altura.toFixed(2));
+    } else if (lado1 == lado2 || lado1 == base || lado2 == base ) {
+        console.log("es isocelesv2");
+        console.log("puede ser rectangulov2");
+        const altura = Math.sqrt((lado1**2) - ((base**2 / 4)));
+        return Number(altura.toFixed(2));
+    } else {
+        console.log("es escalenov2");
+        console.log("puede ser rectangulov2");
+        const semiperimetro = (lado1 + lado2 + base) / 2;
+        console.log(semiperimetro);
+        const altura = (2 / base) * Math.sqrt(semiperimetro * (semiperimetro - base) * (semiperimetro - lado1) * (semiperimetro - lado2));
+        return Number(altura.toFixed(2));
+    }
+}
+
+function calcularAlturaTriangulo() {
+    const inputBase = document.getElementById("InputBaseTriangulo");
+    const base = Number(inputBase.value);
+    const inputLado1 = document.getElementById("InputLado1Triangulo");
+    const lado1 = Number(inputLado1.value);
+    const inputLado2 = document.getElementById("InputLado2Triangulo");
+    const lado2 = Number(inputLado2.value);
+
+    const altura = alturaTriangulo(base, lado1, lado2);
+    alert(altura);
+}
