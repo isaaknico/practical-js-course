f56f5e5b55ce7a4f171c0b1c86df72ab16f3187b
isaaknico <kaasi_96@hotmail.com>
2022-06-14 19:17:22 -0500

    Actualizado uso de cupones en taller #2

--- discounts.js
+++ discounts.js
@@ -6,75 +6,83 @@ function priceWithDiscount(price, discount) {
     return Number(priceWithDiscount.toFixed(2));
 }
 
+/** Funcion que valida si el cupón es valido */
+function isValid(coupon) {
 
+    const couponList = [
+        { name: "CPON10", percentageDiscount: 10 },
+        { name: "CPON20", percentageDiscount: 20 },
+        { name: "PDAY15", percentageDiscount: 15 },
+        { name: "BFIN25", percentageDiscount: 25 } 
+    ];
 
-/* Funcion que muestra en HTML precio con descuento */
+    const couponValid = couponList.find(function(authenticCoupon) {
+        return authenticCoupon.name === coupon;
+    });
+
+    //Otra forma de escribir la validacion
+    // const isAuthenticCoupon = function(authenticCoupon) {
+	//     return authenticCoupon.name === coupon;
+    // };
+    // const couponValid = couponList.find(isAuthenticCoupon);
+
+    return couponValid;
+}
+
+/* Funcion que muestra en HTML precio con descuento y/o cupón */
 function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
     const price = Number(inputPrice.value);
-    
+
     const inputDiscount = document.getElementById("InputDiscount");
     const discount = Number(inputDiscount.value);
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const userCoupon = inputCoupon.value;
+
+    const pResult = document.getElementById("PResult");
+    const pMessage = document.getElementById("PMessage");
 
     //Obtiene precio final con descuento aplicado
     const finalPrice = priceWithDiscount(price, discount);
 
-    /* Reto: */
-    const inputCoupon = document.getElementById("InputCoupon");
-    const coupon = inputCoupon.value;
-
-    
-    const pResult = document.getElementById("PResult");
-
-    if (coupon) {
-        const couponPercentage = isValid(coupon);
-        console.log(couponPercentage);
-        const couponValue = couponPercentage.percentageDiscount;
-        console.log(couponValue);
-    
-        //Obtiene precio definitivo con cupon aplicado
-        const definitivePrice = priceWithCoupon(finalPrice, couponValue);
-        pResult.innerText = "El precio con descuento es: $" + finalPrice + " y el precio con descuento y cupon es: $" + definitivePrice;
-        /* reto */
-    } else {
+    //Si el usuario No ingresó un cupón
+    if (!userCoupon) {
+        //Muestra en HTML precio con descuento
         pResult.innerText = "El precio con descuento es: $" + finalPrice;
+
+    } else {
+        //Si se ingresó un cupon:
+        //Comprueba si el cupon es valido, si lo es lo guarda en var
+        const coupon = isValid(userCoupon);
+
+        //Si el cupón No es válido
+        if (!coupon) {
+            pMessage.innerText = "Cupón: " + userCoupon + " no valido";
+            pMessage.classList.remove("messageCoupon--success");
+            pMessage.classList.add("messageCoupon--error")
+
+            //Muestra en HTML precio con descuento
+            pResult.innerText = "El precio con descuento es: $" + finalPrice;
+
+        } else {
+            //Si el cupón es válido
+            pMessage.innerText = "Cupón: " + userCoupon + " aplicable";
+            pMessage.classList.add("messageCoupon--success");
+            pMessage.classList.remove("messageCoupon--error")
+            
+            const couponPctDiscount = coupon.percentageDiscount;
+    
+            //Obtiene precio definitivo con cupon y descuento aplicado
+            const definitivePrice = priceWithDiscount(finalPrice, couponPctDiscount);
+
+            //Muestra en HTML precio con descuento y cupon
+            pResult.innerText = "El precio con descuento es: $" + finalPrice + " y el precio con descuento y cupon es: $" + definitivePrice;
+        }
     }
-    
-
-    
 }
 
-/* RETO: */
-// Nuestros clientes están muy felices de poder calcular el precio final de sus productos después del descuento,
-// pero la tienda tiene una nueva solicitud para nosotros: implementar descuentos con cupones.
-
-const coupon = "BFD010";
-
-/** Funcion que valida si el cupon es valido */
-function isValid(coupon) {
-    const couponList = [
-        { coupon: "CPON10", percentageDiscount: 10 },
-        { coupon: "CPON20", percentageDiscount: 20 },
-        { coupon: "PDAY15", percentageDiscount: 15 },
-        { coupon: "BFIN25", percentageDiscount: 25 } 
-    ];
-
-    var couponIsValid = couponList.find(function(couponCode) {
-        return couponCode.coupon === coupon;
-    });
-
-    return couponIsValid;
-}
-
-/** Funcion que obtiene precio con cupon */
-function priceWithCoupon(priceWDiscount, coupon) {
-    const discountPricePercentage = 100 - coupon;
-    const priceDefinitive = (priceWDiscount * (discountPricePercentage)) / 100;
-
-    return Number(priceDefinitive.toFixed(2));
-}
-
-/** Funcion que imprime en HTML si cupon es valido */
+/* Funcion que muestra en HTML si el cupón es valido o no */
 function onClickButtonValidateCoupon() {
     const inputCoupon = document.getElementById("InputCoupon");
     const coupon = inputCoupon.value;
@@ -83,13 +91,14 @@ function onClickButtonValidateCoupon() {
 
     const isValidCoupon = isValid(coupon);
 
-    if (isValidCoupon) {
-        pMessage.innerText = "Cupón: " + coupon + " aplicable";
-        pMessage.classList.add("messageCoupon--success");
-        pMessage.classList.remove("messageCoupon--error")
-    } else {
+    if (!isValidCoupon) {
         pMessage.innerText = "Cupón: " + coupon + " no valido";
         pMessage.classList.remove("messageCoupon--success");
-        pMessage.classList.add("messageCoupon--error")
+        pMessage.classList.add("messageCoupon--error");
+
+    } else {
+        pMessage.innerText = "Cupón: " + coupon + " aplicable";
+        pMessage.classList.add("messageCoupon--success");
+        pMessage.classList.remove("messageCoupon--error");
     }
 }
