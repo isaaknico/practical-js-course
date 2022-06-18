f3d7810ca44a0a9ac4b6f253476c81d56bb0d258
isaaknico <kaasi_96@hotmail.com>
2022-06-18 13:52:50 -0500

    Añadí UI a calculo de moda en taller #3

--- mode.js
+++ mode.js
@@ -1,3 +1,6 @@
+/* Define lista de usuario */
+var list = [];
+
 /* Funcion que calcula la moda */
 function calculateMode(list) {
     //Cuenta las veces que aparece cada numero en nuestro array y las guarda en un objeto.
@@ -36,4 +39,61 @@ function calculateMode(list) {
     const mode = modeElem[0];
 
     return mode;
+}
+
+/* Funcion que muestra en HTML lista recorrida */
+function printList(array) {
+    var pResultSum = ``;
+
+    for (let i = 0; i < array.length; i++) {
+        pResultSum += `<li class="list-item"> ${ array[i] } </li>`;
+    }
+
+    const pResult = document.getElementById("UlResult");
+    pResult.innerHTML = pResultSum;
+}
+
+/* Funcion que muestra en HTML elem agregado a la lista, tambien lo inserta en lista */
+function onClickButtonAddElem() {
+    const inputElem = document.getElementById("InputElem");
+    const elem = Number(inputElem.value);
+
+    //Si se ingresó un elem
+    if (elem) {
+        //Añade elem al array
+        list.push(elem);
+
+        //Muestra en HTML lista recorrida con elem agregado
+        printList(list);
+        
+        //Limpia el formulario(input)
+        document.getElementById("Form").reset();
+    }
+}
+
+/* Funcion que borra ultimo elem de lista */
+function onClickButtonDeleteLastElem() {
+    list.pop();
+
+    //Muestra en HTML lista recorrida con elem borrado
+    printList(list);
+}
+
+/* Funcion que reinicia lista */
+function onClickButtonRestartList() {
+    list = [];
+
+    //Muestra en HTML lista recorrida(vacia)
+    printList(list);
+
+    const pResult = document.getElementById("PResult");
+    pResult.innerText = "";
+}
+
+/* Funcion que muestra en HTML la moda */
+function onClickButtonMode() {
+    const mode = Number(calculateMode(list));
+
+    const pResult = document.getElementById("PResult");
+    pResult.innerText = "La moda de tu lista es: " + mode;
 }
