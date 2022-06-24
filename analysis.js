d522f8c2fd0301b4f6741c993c5cb48f12d5019a
isaaknico <kaasi_96@hotmail.com>
2022-06-23 22:44:41 -0500

    Reordené funciones de calculo mediana salarial taller #4

--- analysis.js
+++ analysis.js
@@ -1,5 +1,5 @@
 /* Define lista de salario del usuario */
-const salaryData = [];
+let salaryData = [];
 
 // salaryData.push({
 //     entry: 1500,
@@ -11,53 +11,124 @@ function isPair(number) {
     return (number % 2 === 0);
 }
 
+function mapAndSort(array) {
+    const salaries = array.map(function(elem) {
+        return elem.entry;
+    });
+    
+    const salariesOrdered = salaries.sort(function(salaryA, salaryB) {
+        return salaryA - salaryB;
+    });
 
-const salaries = salaryData.map(function (elem) {
-    return elem.entry;
-});
+    return salariesOrdered;
+}
 
-const salariesOrdered = salaries.sort(function (salaryA, salaryB) {
-    return salaryA - salaryB;
-});
-
-function medianSalary() {
-    const half = parseInt(salariesOrdered.length / 2);
+function medianSalary(array) {
+    const half = parseInt(array.length / 2);
     var median = 0;
     
-    if (isPair(salariesOrdered.length)) {
-        const medianA = salariesOrdered[half - 1];
-        const medianB = salariesOrdered[half];
+    if (isPair(array.length)) {
+        const medianA = array[half - 1];
+        const medianB = array[half];
 
         median = (medianA + medianB) / 2;
         return median;
     } else {
-        median = salariesOrdered[half];
+        median = array[half];
         return median;
     }
 }
 
-function medianSalaryTop10() {
-    const spliceStart = parseInt((salariesOrdered.length * 90) / 100);
-    const spliceCount = salariesOrdered.length - spliceStart;
+// Array debe estar ordenado
+function medianSalaryTop10(array) {
+    const spliceStart = parseInt((array.length * 90) / 100);
+    const spliceCount = array.length - spliceStart;
     
-    const salariesTop10 = salariesOrdered.splice(spliceStart, spliceCount);
-    
+    const salariesTop10 = array.splice(spliceStart, spliceCount);
+    console.log({
+        salariesTop10, spliceStart, spliceCount});
     const medianTop10 = medianSalary(salariesTop10);
     return medianTop10;
 }
 
-function onClickCalculateMedianSalary() {
-    const medianGeneral = medianSalary();
+function printList(array) {
+    var targetTagResult = "";
+    var targetTagContent = "";
+    var tagFullContent = ``;
 
-    const pResult = document.getElementById("p_result");
-    pResult.innerHTML = `La mediana salarial es: ${ medianGeneral }`;
+    switch (array) {
+        case salaryData:
+            targetTagResult = "p_result";
+            targetTagContent = "ul_result-median-salary";
+            for (let i = 0; i < array.length; i++) {
+                tagFullContent +=   `<li class="list-weighted-mean__item">
+                                        <span class="list-weighted-mean__sub-item list-weighted-mean__sub-item--left"> $ ${ array[i].entry } </span>
+                                        <span class="list-weighted-mean__sub-item list-weighted-mean__sub-item--right"> ${ array[i].date } </span>
+                                    </li>`;
+            }
+            break;
+        default:
+            break;
+    }
+    
+    var tagResult = document.getElementById(targetTagResult);
+    tagResult.innerText = "";
+    var tagContent = document.getElementById(targetTagContent);
+    tagContent.innerHTML = tagFullContent;
 }
 
-function onClickCalculateMedianSalaryTop10() {
-    const medianTop10 = medianSalaryTop10();
+/* Funcion que agrega elem en lista(notes) y muestra en HTML lista  */
+function onClickButtonAddElemMS() {
+    const inputEntry = document.getElementById("input_entry");
+    const entry = Number(inputEntry.value);
+    const inputDate = document.getElementById("input-date");
+    const date = inputDate.value;
+
+    // Si se ingresó un elem
+    if (entry) {
+        // Crea obj de calificacion
+        const objNote = { entry: entry, date: date }
+
+        salaryData.push(objNote);
+
+        printList(salaryData);///////imprimir lista salarios
+        
+        // Limpia el formulario(input)
+        document.getElementById("form_median-salary").reset();
+    }
+}
+
+/* Funcion que borra ultimo elem de lista */
+function onClickButtonDeleteLastElem(array) {
+    array.pop();
+
+    printList(array);
+}
+
+/* Funcion que reinicia lista */
+function onClickButtonRestartList(array) {
+    //array = [];
+    array.length = 0;
+
+    printList(array);
+}
+
+function onClickButtonCalculateMedianSalary() {
+    const salariesOrdered = mapAndSort(salaryData);
+
+    const medianGeneral = medianSalary(salariesOrdered);
 
     const pResult = document.getElementById("p_result");
-    pResult.innerHTML = `La mediana salarial del top 10% es: ${ medianTop10 }`;
+    pResult.innerHTML = `La mediana salarial es: $ ${ medianGeneral }`;
+}
+
+function onClickButtonCalculateMedianSalaryTop10() {
+    const salariesOrdered = mapAndSort(salaryData);
+
+    const medianTop10 = medianSalaryTop10(salariesOrdered);
+
+    const pResult = document.getElementById("p_result");
+    pResult.innerHTML = `La mediana salarial del top 10% es: $ ${ medianTop10 }`;
 }
 
 // console.log({
